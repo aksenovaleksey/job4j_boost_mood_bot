3d4b4e9919e1f40642145afed91a182525c9c4af
aksenovaleksey <Aksenov_AV@bk.ru>
2026-03-21 17:29:23 +0300

    DI, IoC, Beans, BeanFactory, ApplicationContext [#505289]

---
+++ src/main/java/ru/job4j/bmb/ContentProvider.java
@@ -0,0 +1,7 @@
+package ru.job4j.bmb;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContentProvider {
+}
---
+++ src/main/java/ru/job4j/bmb/MoodLogRepository.java
@@ -0,0 +1,7 @@
+package ru.job4j.bmb;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MoodLogRepository {
+}
